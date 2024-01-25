11e71cfd2a859c1ccd960c9a4c1eec339cde9e89
Vincent Toon <vincenttoon22@gmail.com>
2024-01-24 21:30:55 -0600

    remove body

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,7 +5,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = await req.json()
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
