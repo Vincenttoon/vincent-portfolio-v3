6497b3fe6e1608f28b62207c9468c2652df9747c
Vincent Toon <vincenttoon22@gmail.com>
2025-08-11 20:18:25 -0500

    bulk reenter

--- app/page.js
+++ app/page.js
@@ -13,13 +13,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <NavBar />
-        <div className="container mt-24 mx-auto px-12 py-4">
-          {/* <HeroSection /> */}
-          {/* <AchievementsSection /> */}
-          {/* <AboutSection /> */}
-          {/* <ProjectsSection /> */}
-          {/* <EmailSection /> */}
-        </div>
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AchievementsSection />
+        <AboutSection />
+        <ProjectsSection />
+        <EmailSection />
+      </div>
       <Footer />
     </main>
   );
