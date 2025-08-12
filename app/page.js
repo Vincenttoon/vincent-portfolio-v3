c713843528dc561c0016f8898053d63c65a38b06
Vincent Toon <vincenttoon22@gmail.com>
2025-08-11 20:47:01 -0500

    projects readded and emails fixed

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
         <HeroSection />
         <AchievementsSection />
         <AboutSection />
-        {/* <ProjectsSection /> */}
+        <ProjectsSection />
         <EmailSection />
       </div>
       <Footer />
