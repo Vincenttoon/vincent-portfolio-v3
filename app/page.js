3e31c04168a8acfc0d838fac97f596624c313229
Vincent Toon <vincenttoon22@gmail.com>
2025-08-11 20:27:03 -0500

    achievements readded

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
-        {/* <AchievementsSection /> */}
+        <AchievementsSection />
         {/* <AboutSection /> */}
         {/* <ProjectsSection /> */}
         <EmailSection />
